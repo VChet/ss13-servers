d1de03a988fb0935aef6a781e9789a88f17153b7
VChet <17050347+VChet@users.noreply.github.com>
2019-10-13 22:13:39 +0500

    add third tau server, show game mode

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ const getServerData = function(url, blockId) {
   getJSON(url, function(error, data) {
     if (error) return console.error({ error });
     const dataBlock = document.querySelector(blockId);
-    dataBlock.querySelector(".servers__map").innerText = data.map;
+    dataBlock.querySelector(".servers__mode").innerText = `${data.map} (${data.mode})`;
     dataBlock.querySelector(".servers__players").innerText = `${data.players} ${declOfNum(data.players, ["игрок", "игрока", "игроков"])}`;
     dataBlock.querySelector(".servers__roundTime").innerText = `Продолжительность: ${data.roundduration}`;
   });
@@ -31,6 +31,7 @@ const getServerData = function(url, blockId) {
 const updateTauServers = function() {
   getServerData("https://taucetistation.org/server/tauceti/json", "#TC1");
   getServerData("https://taucetistation.org/server/tauceti2/json", "#TC2");
+  getServerData("https://taucetistation.org/server/tauceti3/json", "#TC3");
 }
 
 document.addEventListener("DOMContentLoaded", function() {
