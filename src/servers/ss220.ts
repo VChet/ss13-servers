8e165245ed48cc66619f7019c65531f4a1cf8934
VChet <17050347+VChet@users.noreply.github.com>
2025-10-06 16:52:23 +0500

    chore(ss220): update servers

--- src/servers/ss220.ts
+++ src/servers/ss220.ts
@@ -3,28 +3,28 @@ import type { ServerInfo } from "@/types/server";
 
 export const ss220Links: Link[] = [
   { text: "Discord", icon: "discord", url: "https://discord.gg/ss220" },
-  { text: "Правила ", icon: "rules", url: "https://wiki.ss220.club/index.php/Правила_Сервера" },
-  { text: "Вики", icon: "wiki", url: "https://wiki.ss220.club" },
+  { text: "Правила ", icon: "rules", url: "https://bs.ss220.club/index.php/Правила" },
+  { text: "Вики", icon: "wiki", url: "https://bs.ss220.club" },
   { text: "Вики Prime", icon: "wiki", url: "https://wiki.ss220.club/index.php/Раздел_Prime_сервера" }
 ];
 
 export const ss220Servers: ServerInfo[] = [
   {
-    name: "Green",
+    name: "Black",
+    description: "Тестовый сервер",
     build: "Paradise",
-    map: "Box Station (Cyberiad)\nDeltaStation (Kerberos)\nMetaStation (Cerebron)",
+    url: "byond://s4.ss220.club:4000"
+  },
+  {
+    name: "Green",
+    description: "Стабильный сервер",
+    build: "Paradise",
     url: "byond://s1.ss220.club:4002"
   },
   {
-    name: "Black",
-    build: "Paradise",
-    map: "Box Station (Cyberiad)\nDeltaStation (Kerberos)\nMetaStation (Cerebron)",
-    url: "byond://s1.ss220.club:4000"
-  },
-  {
     name: "Prime",
-    build: "Paradise",
-    map: "Box Station (Cyberiad)\nDeltaStation (Kerberos)\nMetaStation (Cerebron)",
-    url: "byond://s1.ss220.club:4001"
+    description: "Сервер с вайтлистом для стримеров",
+    build: "/tg/",
+    url: "byond://116.202.156.155:3254"
   }
 ];
